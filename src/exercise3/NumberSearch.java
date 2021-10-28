4ed80dc35fe5268d0a6a7bef950924d242b98636
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2021-10-28 08:55:55 +0300

    Ubung 3

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -3,20 +3,33 @@ package exercise3;
 public class NumberSearch {
     public static void main(String[] args) {
         int number;
-        for (number = 100; number <1000; number++) {
-            if (number%5==0 && number %6==0) {
+        int counter = 0;
+        for (number=100; number<1000; number++) {
+            if (number%5==0 && number%6==0) {
                 System.out.print(number + " ");
-                if (number == 390){
+                counter++;
+                if (counter%10==0) {
                     System.out.println();
-                    }
-                if (number == 690){
-                    System.out.println();
-                }
-                if (number == 990){
-                    System.out.println();
-                }
                 }
             }
         }
+
+
     }
+
+}
+
+
+
+
+    /*for (number = 100; number <1000; number++) {
+
+        if (number%5==0 && number %6==0) {
+        System.out.print(number + " ");
+
+        if (counter %10 == 1){
+        System.out.println();
+        }
+        }
+        }*/
 
